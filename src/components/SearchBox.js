17379ca2672682da1d01bf4cb56c1be5648f95b9
Daniel Frick <danielfrick7@gmail.com>
2018-07-22 09:26:45 -0500

    updating robofriends with redux

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,9 +4,9 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return(
     <div className='pa2'>
       <input
-        className='pa3 ba b--green bg-lightest blue'
+        className='pa3 ba b--green bg-lightest-blue'
         type='search'
-        placeholer='search robots'
+        placeholder='search robots'
         onChange = { searchChange }
       />
     </div>
